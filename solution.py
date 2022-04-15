6224ce89dbf4ab3c43e7ab8008948dce452c264d
Piyumnyu <pkm5782@nyu.edu>
2022-04-15 15:12:10 +0530

    4

--- solution.py
+++ solution.py
@@ -131,7 +131,7 @@ def ping(host, timeout=1):
     #timeout=1 means: If one second goes by without a reply from the server,
     #the client assumes that either the client's ping or the server's pong is lost
     dest = socket.gethostbyname(host)
-    print "Pinging " + dest + " using Python:"
+    print ("Pinging " + dest + " using Python:")
     #Send ping requests to a server separated by approximately one second
     try:
         while True:
